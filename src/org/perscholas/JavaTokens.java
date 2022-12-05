7502154c2929c5133d5424016dda7c2a55eddecf
Nesibe Karatas <nsbkaratas@gmail.com>
2022-12-04 21:29:37 -0500

    tokens solved

--- src/org/perscholas/JavaTokens.java
+++ src/org/perscholas/JavaTokens.java
@@ -7,9 +7,21 @@ public class JavaTokens {
 	public static void main(String[] args) {
 		 Scanner scan = new Scanner(System.in);
 		 System.out.println("enter a sentence:");
-	        String s = scan.nextLine();
+	     String s = scan.nextLine();
 	        
-	        // Write your code here.
+         String[] tokens = s.split("[^a-zA-Z]");
+         int numTokens = 0;
+        
+         for (int i=0; i<tokens.length; ++i) {
+             if (tokens[i].length() > 0) 
+                 numTokens++;
+         }  
+         System.out.println(numTokens);
+        
+         for (int i=0; i<tokens.length;++i) {
+             if (tokens[i].length() > 0)
+                 System.out.println(tokens[i]);
+         }  
 	        scan.close();
 
 	}
